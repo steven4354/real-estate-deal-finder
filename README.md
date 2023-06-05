af8c15c44b54d3e32c28b186b3bbbdf0705e6e47
steven4354 <steven4354@gmail.com>
2023-06-04 18:55:24 -0700

    wip

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # real-estate-deal-finder
-throwing some specs on how to find the optimal solutions for SFH/APT investment deals in a given area
+
+## why?
+
+You have a list of redfin properties.
+
+You spend hours scrolling through their list to find the property you want.
+
+Wouldn't it be better if the properties were already "ranked" for you by the things you are interested in? - closeness to downtown, price, square feet, others
+
+Using this you can import in redfin.csv properties and have it ranked for you.
+
+You can also assign a "weight" to each property feature based on how important the feature is to you. For example if price is the #1 factor, put a larger weight for it.
+
+## how it works
+
+1. Put your redfin.csv export in `/dataset`
+
+2. Get an empty postgres database and add the creds to a `.env` file. See `.env.example`.
+
+3. Run "npm start"
+
+## demo
+
+You can see a working version of this at https://ebadecc052a86d67.dynaboard.app/
--- rank-properties-example.sql
+++ rank-properties-example.sql
@@ -19,7 +19,7 @@ WITH location_data AS (
 distance_data AS (
 	SELECT
 		address,
-		SQRT(POW(CAST(latitude AS FLOAT) - 37.7749,
+		SQRT(POW(CAST(latitude AS FLOAT) - 29.716944,
 				2) + POW(CAST(longitude AS FLOAT) - (- 122.4194),
 				2)) AS distance
 	FROM
@@ -32,9 +32,9 @@ score_data AS (
 		location_data.state_or_province,
 		location_data.zip_or_postal_code,
 		distance,
-		ABS(CAST(price AS INTEGER) - 1000000) AS price_difference,
+		ABS(CAST(price AS INTEGER) - 1) AS price_difference,
 		ABS(COALESCE(price_per_sqft,
-				0) - 1000) AS price_per_sqft_difference
+				0) - 1) AS price_per_sqft_difference
 	FROM
 		distance_data
 		JOIN location_data USING (address)
@@ -53,3 +53,17 @@ FROM
 ORDER BY
 	score ASC
 LIMIT 10;
+
+/*
+target_latitude, default value: 29.716944
+target_longitude, default value: -95.402778
+target_price, default value: 1000000 ($1,000,000)
+target_price_per_sqft, default value: 1000 ($1,000 per square foot)
+distance_weight, default value: 0.5 (weights add up to 1)
+price_weight, default value: 0.3
+price_per_sqft_weight, default value: 0.2
+
+(put at 1 to indicate lower prices is better)
+target_price_per_sqft, default value: 1
+target_price, default value:  1
+*/
